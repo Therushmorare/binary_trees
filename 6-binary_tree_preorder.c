3165d8d3d4cc3a9b5b3438b97b1de6e8daeeec95
tshegofatso <tshego.tt.morare@gmail.com>
2023-11-01 15:05:14 -0700

    task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - pre-order traversal function
+ * @tree: pointer to tree
+ * @ func: pointer to func
+ *
+ * Return: nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
