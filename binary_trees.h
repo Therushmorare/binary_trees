b6d753772c53601f5670a505452400b069210272
tshegofatso <tshego.tt.morare@gmail.com>
2023-11-01 13:38:03 -0700

    header file

---
+++ binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Data structures */
+
+/**
+ * struct binary_tree_s - Binary tree
+ * @n: node integer
+ * @parent: parent node
+ * @left: left child node
+ * @right: right child node
+ */
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+#endif /* BINARY_TREES_H */
