d149fd0056ad57b6140f0682cb76992e8b87c81e
tshegofatso <tshego.tt.morare@gmail.com>
2023-11-02 15:18:55 -0700

    fixed num 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,12 +13,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t left_height = 0, right_height = 0;
 
-		left_height = binary_tree_height(node->left);
-		right_height = binary_tree_height(node->right);
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
 
 		if (left_height > right_height)
 			return (left_height + 1);
 		else
 			return (right_height + 1);
 	}
+	return (0);
 }
